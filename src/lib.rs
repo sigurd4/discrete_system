cc57da4c56eeedb7e713c4cd519ff899cb7226ac
Sigurd <sigurd-s@online.no>
2023-06-23 21:54:29 +0200

    Remove unused features

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-#![feature(trait_alias)]
-#![feature(associated_const_equality)]
 #![feature(generic_const_exprs)]
 #![feature(specialization)]
-#![feature(iter_array_chunks)]
 
 moddef::pub_flat_mods!(siso simo miso mimo);
